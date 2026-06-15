e1265a2cc7b95975f4b064e2f10109cb38f2e6b4
Timur Makarov <public@timurmakarov.com>
2026-06-15 21:55:40 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,15 +31,15 @@ const Home = () => (
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         css={{ backdropFilter: 'blur(10px)' }}
       >
-        Hello! ¡Hola! Привет! გამარჯობა!
+        Hello! ¡Hola! Привет!
       </Box>
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
-            Makarov Timur
+            Timur Makarov
           </Heading>
-          <p>Software Engineer</p>
+          <p>Blockchain Software Engineer</p>
         </Box>
         <Box
           flexShrink={0}
@@ -68,133 +68,11 @@ const Home = () => (
         </Box>
       </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          About
-        </Heading>
-        <Paragraph>
-          I have 3+ years of experience as an engineer, teacher and fan of software.
-          I turn ideas and tasks into elegant backend structures with modern user interfaces.  
-          Technology agnostic: I use and quickly learn those tools that are needed
-          <br></br>
-          I'm highly motivated to learn more and live a better life. So, when I'm not
-          working, you'll find me studying new technologies, reading books or being present, watching my thoughts come and go.
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Experience
-        </Heading>
-        <BioSection>
-          <BioYear>Artificy</BioYear>
-          <span style={{ display: "block", "margin-bottom": "5px" }}>May 2023 - Now</span>
-          <p style={{ textIndent: 0 }}>              
-              - Backend development in Go/PHP
-                <br />
-              - Frontend development
-                <br />
-              - Development of tools for data collection and processing
-                <br />
-              - Development of services based on ML in Python
-              <br />
-              <br />
-          </p>
-        </BioSection>
-        <BioSection>
-          <BioYear>Cruzo</BioYear>
-          <span style={{ display: "block", "margin-bottom": "5px" }}>July 2022 - May 2023</span>
-          <p style={{ textIndent: 0 }}>              
-              - Backend development
-                <br />
-              - Web3 ingeration
-                <br />
-              - Introducing external blockchain solutions
-                <br />
-              - Create user interface and complex animations
-                <br />
-              - Project maintenance, expansion and scaling
-                <br />
-              - Set up and integrate SEO and Analytics
-              <br />
-              <br />
-          </p>
-        </BioSection>
-        <BioSection>
-          <BioYear>Madely</BioYear>
-          <span style={{ display: "block", "margin-bottom": "5px" }}>May 2021 - July 2022</span>
-          <p style={{ textIndent: 0 }}>
-              - Backend development
-                <br />
-              - Development of tools to collect and work with data
-                <br />
-              - Design and creation of user interfaces
-                <br />
-              - Set up and integrate SEO and Analytics
-          </p>
-        </BioSection>
-      </Section>
-
       <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <p>
-          <text style={{ color: 'pink' }}>Software deveploment</text>, Athletics, Finance, <text style={{ color: 'pink' }}>Technology</text> and Books
-        </p>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <Flex>
-          <Box>
-            <Link href="https://github.com/PowderDev" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @PowderDev
-              </Button>
-            </Link>
-          </Box>
-          <Box>
-            <Link href="https://www.linkedin.com/in/makarovt/" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoLinkedin />}
-              >
-                @Makarovt
-              </Button>
-            </Link>
-          </Box>
-          {/* <Box>
-            <Link href="mailto:makarov-timyr@ya.ru" target="_blank">
-              <Button variant="ghost" colorScheme="teal" leftIcon={<IoMail />}>
-                makarov-timyr@ya.ru
-              </Button>
-            </Link>
-          </Box> */}
-
-          {/* <Box>
-            <Link
-              href="https://geeklink.io/members/makarovtimur"
-              target="_blank"
-            >
-              <Button variant="ghost" colorScheme="teal" leftIcon={<IoLink />}>
-                @MakarovTimur
-              </Button>
-            </Link>
-          </Box> */}
-        </Flex>
-
         <Box align="center" my={4}>
           <Button colorScheme="teal">
-            <a href="https://drive.google.com/file/d/1XDE0Zjyt3km7dcolK444ncWmlpQj-Yyx/view?usp=sharing">
-              Download Resume
+            <a href="https://tinyurl.com/timur-makarov-cv-pdf">
+              Resume
             </a>
           </Button>
         </Box>
